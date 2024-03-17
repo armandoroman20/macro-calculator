b53c5caae8d91f3705731a1a84e2b676c7b5f008
Armando <armando.roman.jr20@gmail.com>
2024-03-17 16:53:09 -0500

    Adding the add new item form

--- index.js
+++ index.js
@@ -104,13 +104,23 @@ $(document).ready(function () {
 });
 
 
-// $(document).ready(function () {
-//   // Event handler for toggling between forms
-//   $("#showNewItemFormButton, #showCalculatorFormButton").on("click", function () {
-//     // Toggle visibility of forms
-//     $("#currentForm, #newItemForm").toggle();
-//   });
-// });
+$(document).ready(function () {
+  // Event handler for toggling between forms
+  $("#showNewItemFormButton").on("click", function () {
+    // Toggle visibility of forms
+    $("#calculatorForm").addClass("hide").removeClass("show")
+    $("#newItemForm").addClass("show").removeClass("hide");
+    console.log("add new item form button clicked");
+
+  });
+
+    $("#showCalculatorFormButton").on("click", function () {
+    // Toggle visibility of forms
+    $("#calculatorForm").addClass("show").removeClass("hide")
+    $("#newItemForm").addClass("hide").removeClass("show");
+    console.log("calc form button clicked");
+  });
+});
 
 
     // Event handler for saving the new item
