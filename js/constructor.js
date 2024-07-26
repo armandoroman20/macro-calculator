829de4c8ac30379cb644f9fdd9ab03c435cc2c36
Armando <armando.roman.jr20@gmail.com>
2024-07-26 12:20:37 -0500

    added 2 new foods

--- js/constructor.js
+++ js/constructor.js
@@ -21,7 +21,10 @@ const foods = {
   optimumNutritionChocolate: new Food("Optimum Nutrition Exteme Milk Chocolate Protein", 32, 120, 24, 3, 2),
   borrachoBeans: new Food("H-E-B Borracho Beans", 130, 90, 5, 15, 1.5),
   blackBeans: new Food("H-E-B Black Beans", 130, 100, 7, 19, 0),
-  crinkleCutFries: new Food("H-E-B Crinkle Cut Fries", 84, 100, 2, 16, 3)
+  crinkleCutFries: new Food("H-E-B Crinkle Cut Fries", 84, 100, 2, 16, 3),
+  gardenSaladBlend: new Food("H-E-B Garden Salad", 85, 15, 1, 3, 0),
+  lighBalsamic: new Food("H-E-B Ligh Balsamic", 30, 15, 0, 2, .5)
+
 
   // Add more food objects here as needed
 };
